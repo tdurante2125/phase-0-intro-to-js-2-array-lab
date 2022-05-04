46192ac12bb89475f81493125df4a7dbac6d5a85
Tim Durante <tdurante2125@gmail.com>
2022-05-04 18:14:08 -0400

    commit tree 23e1aa04bdd9679fd435b011475a0de1d9289715

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+cats.push("Ralph");
